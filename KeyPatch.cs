d1f61e2ee3d9ca7cef7edcbe470caa4133d6f7c1
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-05-04 19:23:41 -0300

    Added controller support

--- KeyPatch.cs
+++ KeyPatch.cs
@@ -8,17 +8,32 @@ namespace MBAutoRun
     public class KeyPatch
     {
 
+        private static int KEY_FORWARD  = 0;  // keyboard W
+        private static int KEY_BACKWARD = 1; // keyboard S
+
         public static void Postfix(Key __instance, ref bool __result)
         {
             if (!AutoRunHotKey.IsKeyActive || !__instance.IsKeyboardInput) return;
 
             // If "S" is pressed we want to ignore auto-run so the player can walk backwards
-            GameKey sKey = GenericGameKeyContext.Current.GameKeys.Find((GameKey g) => g.PrimaryKey.InputKey == InputKey.S);
-            if (sKey.PrimaryKey.InputKey.IsDown()) return;
+            GameKey backward = _getKey(KEY_BACKWARD);
+            if (_isKeyDown(backward)) return;
 
-            if (__instance.InputKey == InputKey.W)
+            GameKey forward = _getKey(KEY_FORWARD);
+
+            if ( (__instance.IsControllerInput && __instance.InputKey == forward.ControllerKey.InputKey) ||
+                 __instance.InputKey == forward.PrimaryKey.InputKey)
                 __result = true;
         }
 
+        private static GameKey _getKey(int id)
+        {
+            return GenericGameKeyContext.Current.GameKeys.Find((GameKey g) => g.Id == id);
+        }
+
+        private static bool _isKeyDown(GameKey key)
+        {
+            return key.PrimaryKey.InputKey.IsDown() || key.ControllerKey.InputKey.IsDown();
+        }
     }
 }
